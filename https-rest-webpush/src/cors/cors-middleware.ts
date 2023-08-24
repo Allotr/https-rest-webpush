6280b44750c5b5193bd6ed780f60f983662c78b1
rafaelpernil2 <mysterysynthman@gmail.com>
2023-08-24 17:46:41 +0200

    fix: cors and path

--- https-rest-webpush/src/cors/cors-middleware.ts
+++ https-rest-webpush/src/cors/cors-middleware.ts
@@ -6,7 +6,7 @@ function initializeCORS(app: Express) {
     const corsOptions = {
         origin: (origin, next) => {
             // Test for main domain and all subdomains
-            if (origin == null || origin === 'https://allotr.eu' || /^https:\/\/\w+?\.allotr\.eu$/gm.test(origin)) {
+            if (origin == null || origin === 'https://allotr.eu' || /^https:\/\/.+?\.allotr\.eu$/gm.test(origin)) {
                 next(null, true)
             } else {
                 next(new Error('Not allowed by CORS'))
